9a48485f65ebd4227bd55054fac44049ecce3f11
Vic0309 <tiwner@gmail.com>
2026-04-14 00:17:27 +0800

    Update convert-lpmm-lpph.js

--- js/modules/convert-lpmm-lpph.js
+++ js/modules/convert-lpmm-lpph.js
@@ -12,12 +12,14 @@ export const moduleData = {
                 <input type="number" id="pixel_size" value="2.9">
             </div>
             <div class="calc-group">
-                <label>Sensor Size (Heigh): </label>
+                <label>Sensor Size (Height): </label>
                 <input type="number" id="h_val" value="1080">
             </div>
             <button id="run-btn">Calculation</button>
             <div id="res-box" style="margin-top:20px; font-weight:bold; color:#3498db;"></div>
-            <div id="res-box-PH" style="margin-top:20px; font-weight:bold; color:#3498db;"></div>
+            <div id="res-box-mm" style="margin-top:20px; font-weight:bold; color:#3498db;"></div>
+            <div id="res-box-ph" style="margin-top:20px; font-weight:bold; color:#3498db;"></div>
+            
         `;
 
         // 綁定計算邏輯
@@ -25,9 +27,11 @@ export const moduleData = {
             const pixel_size = parseFloat(document.getElementById('pixel_size').value);
             const image_h = parseFloat(document.getElementById('h_val').value);
             const res_mm = 1 / (2 * pixel_size/1000);
-            const ph = image_h * pixel_size
+            const ph = image_h * pixel_size /1000
             const res_ph = res_mm * ph;
-            document.getElementById('res-box').innerText = `Sensor奈奎斯特最大解析度: ${res_mm.toFixed(2)} LP/mm = ${res_ph.toFixed(2)} LP/PH`;
+            document.getElementById('res-box').innerText = `Sensor奈奎斯特最大解析度: ${res_mm.toFixed(2)} LP/mm = ${res_ph.toFixed(1)} LP/PH`;
+            document.getElementById('res-box-mm').innerText = `${res_mm.toFixed(2)} LP/mm，在1mm空間中有${res_mm.toFixed(2)}Line Pair`
+            document.getElementById('res-box-ph').innerText = `${res_ph.toFixed(1)} LP/PH，在影像高度${ph.toFixed(1)}mm空間中有${res_ph.toFixed(1)}Line Pair`
         };
     }
 };
