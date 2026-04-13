4b270180e97019160eb152c8a8946b0f41640c00
Vic0309 <tiwner@gmail.com>
2026-04-13 23:49:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,8 @@ export const toolsConfig = [
         category: "影像公式",
         items: [
             { id: "rgb-yuv", title: "RGB 轉 YUV", fileName: "rgb-yuv.js" },
-            { id: "fov-calc", title: "鏡頭 FOV 計算", fileName: "fov-calc.js" }
+            { id: "fov-calc", title: "鏡頭 FOV 計算", fileName: "fov-calc.js" },
+            { id: "convert-lpmm-lpph", title: "Sensor最大解析度計算", fileName: "Covert_LPMM_LPPH.js" }
         ]
     },
     {
