e0b46f57a2decec18b706699180e6b69750690c5
Vic0309 <tiwner@gmail.com>
2026-04-26 23:12:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const toolsConfig = [
             { id: "rgb-yuv", title: "RGB 轉 YUV", fileName: "rgb-yuv.js" },
             { id: "fov-calc", title: "鏡頭 FOV 計算", fileName: "fov-calc.js" },
             { id: "convert-lpmm-lpph", title: "Sensor最大解析度計算", fileName: "convert-lpmm-lpph.js" }
-            { id: "sensitivity_calculation", title: "相機最大感光度計算 (Sensitivity / Minimum Illumination)", fileName: "sensitivity_calculation.js" }
+            { id: "sensitivity-calculation", title: "相機最大感光度計算 (Sensitivity / Minimum Illumination)", fileName: "sensitivity-calculation.js" }
         ]
     },
     {
